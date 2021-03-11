cf481a7d01a8772e56295dccf1d9c1d44bb0cb7f
salsanchez004 <75338728+salsanchez004@users.noreply.github.com>
2021-03-10 20:44:54 -0600

    Add files via upload

---
+++ php/SendComment.php
@@ -0,0 +1,18 @@
+<?php
+$destino = "ansanchez@tamayoconsultoria.com";
+$filename="index.php";
+$nombre = $_POST["nombre"];
+$inmail = $_POST["email"];
+$lugar = $_POST["origen"];
+$mensaje = $_POST["comentario"];
+$contenido = "Nombre: " . $nombre . "\nCorreo: " . $inmail . "\nMensaje: " . $mensaje;
+mail($destino, "Contacto", $contenido);
+echo '<script type="text/javascript">';
+echo 'window.location.href="'.$filename.'";';
+echo 'alert("Su mensaje se esta enviando...");';
+echo '</script>';
+echo '<noscript>';
+echo '<meta http-equiv="refresh" content="0;url='.$filename.'" />';
+echo '</noscript>';
+die();
+?>
---
+++ php/correo.php
@@ -0,0 +1,19 @@
+﻿<?php
+$destino = "e.proyectocuratorial@gmail.com";
+$filename="https://paroxismoexpo.com";
+$nombre = $_POST["nombre"];
+$edad = $_POST["edad"];
+$lugar = $_POST["origen"];
+$mensaje = $_POST["mensaje"];
+$contenido = "Nombre: " . $nombre . "\nEdad: " . $edad . "\nLugar de origen: " . $lugar . "\nMensaje: " . $mensaje;
+mail($destino, "Contacto", $contenido);
+//header('Location: http://paroxismoexpo.com');
+echo '<script type="text/javascript">';
+echo 'window.location.href="'.$filename.'";';
+echo 'alert("Su mensaje se esta enviando...");';
+echo '</script>';
+echo '<noscript>';
+echo '<meta http-equiv="refresh" content="0;url='.$filename.'" />';
+echo '</noscript>';
+die();
+?>
